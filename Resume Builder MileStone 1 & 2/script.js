d5ba1663196d5fded78d9aaabe4fc53a012b0947
OsamabinAdnan <binadnanosama@gmail.com>
2024-09-08 16:21:21 +0500

    Milestone 1 and 2

---
+++ Resume Builder MileStone 1 & 2/script.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const toggleSkillsButton = document.getElementById('toggle-skills');
+let skillsSection = document.getElementById('skills');
+toggleSkillsButton?.addEventListener('click', () => {
+    if (skillsSection) {
+        if (skillsSection.style.display === 'none') {
+            skillsSection.style.display = 'block';
+            toggleSkillsButton.textContent = 'Hide Skills';
+        }
+        else {
+            skillsSection.style.display = 'none';
+            toggleSkillsButton.textContent = 'Show Skills';
+        }
+    }
+});
